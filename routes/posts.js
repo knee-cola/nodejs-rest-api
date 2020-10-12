63908df2121686079e071b1c04058c455a48d31a
knee-cola <nikola.derezic@gmail.com>
2020-10-12 15:25:31 +0200

    added DELETE

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const { restart } = require('nodemon');
 const router = express.Router();
 const Post = require('../model/Post');
 
+// GET ALL POSTS
 router.get('/', async (res,resp) => {
     try {
         const posts = await Post.find();
@@ -14,6 +15,7 @@ router.get('/', async (res,resp) => {
     }
 });
 
+// ADD NEW POST
 router.post('/', async (req,resp) => {
     
     const post = new Post({
@@ -25,6 +27,30 @@ router.post('/', async (req,resp) => {
         const savedPost = await post.save();
     
         resp.status(200).json(savedPost);
+    } catch (ex) {
+        resp.status(500).json({
+            ex
+        });
+    }
+});
+
+// GET A SPECIFIC POST (byId)
+router.get('/:postId', async (req, resp) => {
+    try {
+        const posts = await Post.find({_id: req.params.postId});
+        resp.status(200).json(posts);
+    } catch (ex) {
+        resp.status(500).json({
+            ex
+        });
+    }
+});
+
+// DELETE A SPECIFIC POST (byId)
+router.delete('/:postId', async (req, resp) => {
+    try {
+        const deleteResult = await Post.deleteOne({_id: req.params.postId});
+        resp.status(200).json(deleteResult);
     } catch (ex) {
         resp.status(500).json({
             ex
